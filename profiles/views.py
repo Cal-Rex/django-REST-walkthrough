5595f1dbad26d4f75e33fb193c6912df5149b3ae
Cal-Rex <andy.gibb@healthcareskills.com>
2023-08-27 17:28:47 +0000

    update fields in profileDetail view to take serializer data

--- profiles/views.py
+++ profiles/views.py
@@ -57,8 +57,28 @@ class ProfileDetail(generics.RetrieveUpdateAPIView):
     Retrieve or update a profile if you're the owner.
     """
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Profile.objects.all()
+    # queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True),
+    ).order_by('-created_at')
+    filter_backends = [
+        filters.OrderingFilter,
+        DjangoFilterBackend,
+    ]
+    ordering_fields = [
+        'post_count',
+        'followed_count',
+        'following_count',
+        'owner__followed__created_at',
+        'owner__following__created_at',
+    ]
+    filterset_fields = [
+        'owner__following__followed__profile',
+        'owner__followed__owner__profile',
+    ]
 
 
 # class ProfileDetail(APIView):
